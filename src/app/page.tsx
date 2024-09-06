e9c48d83d3c08b6d48cbd5080ce146d69de526b3
Miguel Angel Bermeo Otavo <mbermeo@upstart13.com>
2024-09-05 20:14:31 -0500

    Add prettier and the initial app logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,91 @@
-import Image from "next/image";
+'use client';
+import React, { useState } from 'react';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+interface Pair {
+    giver: string;
+    receiver: string | null;
 }
+
+const Home: React.FC = () => {
+    const [maleCharacters] = useState<string[]>(['Sebas', 'JuanMan', 'Julian', 'Wilfran', 'Pedro', 'Jhon', 'Andres', 'Rodri']);
+
+    const [femaleCharacters] = useState<string[]>(['Yohanna', 'Wendy', 'Aleja', 'Erika']);
+    const [pairs, setPairs] = useState<Pair[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const shuffleArray = <T,>(array: T[]): T[] => {
+        return array.sort(() => Math.random() - 0.5);
+    };
+
+    const generatePairs = () => {
+        setLoading(true);
+        setTimeout(() => {
+            const shuffledMales = shuffleArray([...maleCharacters]);
+            let shuffledFemales = shuffleArray([...femaleCharacters]);
+
+            const allParticipants = shuffleArray([...shuffledMales, ...shuffledFemales]);
+            let remainingParticipants = [...shuffledMales];
+
+            const newPairs: Pair[] = [];
+
+            for (let i = 0; i < allParticipants.length; i++) {
+                const giver = allParticipants[i];
+                let receiver: string | null = null;
+
+                if (femaleCharacters.includes(giver)) {
+                    const availableMales = remainingParticipants.filter((male) => male !== giver);
+                    receiver = availableMales.pop() || null;
+                    remainingParticipants = remainingParticipants.filter((male) => male !== receiver);
+                } else {
+                    const availableReceivers = remainingParticipants.concat(shuffledFemales).filter((person) => person !== giver);
+                    receiver = availableReceivers.pop() || null;
+                    remainingParticipants = remainingParticipants.filter((person) => person !== receiver);
+                    shuffledFemales = shuffledFemales.filter((person) => person !== receiver);
+                }
+
+                newPairs.push({ giver, receiver });
+            }
+
+            setPairs(newPairs);
+            setLoading(false);
+        }, 3000);
+    };
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+            <h1 className="md:text-4xl text-2xl font-bold text-blue-600 mb-6">Sorteo de Amigo Secreto</h1>
+
+            <button onClick={generatePairs} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-6">
+                Descubre quien es tu amigo secreto!
+            </button>
+
+            {loading && (
+                <div className="text-center flex flex-col items-center justify-center text-lg text-gray-700">
+                    <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-500 mb-4"></div>
+                    ¡Realizando sorteo!
+                </div>
+            )}
+
+            {!loading && pairs.length > 0 && (
+                <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-lg">
+                    <h2 className="text-2xl font-semibold text-gray-800 mb-4">Resultados del Sorteo:</h2>
+                    <ul className="list-disc pl-5 space-y-2">
+                        {pairs.map((pair, index) => (
+                            <li key={index} className="text-lg text-gray-700">
+                                <span className="pr-2 rounded">
+                                    <strong>{pair.giver}:</strong>
+                                </span>
+                                tu amigo secreto es
+                                <span className="pl-1 rounded">
+                                    <strong>{pair.receiver}</strong>
+                                </span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Home;
